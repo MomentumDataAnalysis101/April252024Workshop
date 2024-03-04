1b9d1a7b28553658ca4b7f76843e9e2ab3049cc4
Winnie Ng <winmax@shaw.ca>
2024-03-03 22:19:23 -0700

    Add folder Data
    Folder contains material for the data portion of the workshop

---
+++ Data/Northwind Database Scripts/2. Insert Categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO "Categories"("CategoryID","CategoryName","Description","Picture") 
+VALUES(1,'Beverages','Soft drinks, coffees, teas, beers, and ales',null),
+(2,'Condiments','Sweet and savory sauces, relishes, spreads, and seasonings',null),
+(3,'Confections','Desserts, candies, and sweet breads',null),
+(4,'Dairy Products','Cheeses',null),
+(5,'Grains/Cereals','Breads, crackers, pasta, and cereal',null),
+(6,'Meat/Poultry','Prepared meats',null),
+(7,'Produce','Dried fruit and bean curd',null),
+(8,'Seafood','Seaweed and fish',null);
